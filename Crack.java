b8afc64be83f2a87c5d17e8ea942a2b98da85e63
Vance Miller <vbmiller@cs.unc.edu>
2014-12-15 14:50:54 -0500

    method templates

--- Crack.java
+++ Crack.java
@@ -5,6 +5,12 @@ public class Crack {
 
 
     public static void main(String[] args) {
+        byte[] hash = {0x9, 0x8, 0xa, 0x2, 0x9, 0x6, 0x6, 0xd, 0x4, 0xd, 0x8, 0x7, 0xb, 0x3, 0xb, 0x6, 0x2, 0xb, 0x2,
+                0xb, 0x9, 0x2, 0x4, 0x1, 0xe, 0x3, 0xd, 0x5, 0x1, 0x7, 0xf, 0xf};
+
+        if (args.length == 1) {
+            hash = toByteArray(args[0]);
+        }
         // create and initialize the password string
         char[] candidate = new char[NUM_CHARS];
         for (int i = 0; i < candidate.length; i++) {
@@ -18,13 +24,39 @@ public class Crack {
         }
 
         // for each character in the password
-        for (int i = 0; i < candidate.length; i++) {
-            //
-        }
+        do {
+            candidate[1] = START;
+            do {
+                candidate[2] = START;
+                do {
+                    candidate[3] = START;
+                    do {
+                        candidate[4] = START;
+                        do {
+                            candidate[5] = START;
+                            if (matches(toHashString(candidate), hash)) ;
+                            candidate[5]++;
+                        } while (candidate[5] != START);
+                        candidate[4]++;
+                    } while (candidate[4] != START);
+                    candidate[3]++;
+                } while (candidate[3] != START);
+                candidate[2]++;
+            } while (candidate[2] != START);
+            candidate[1]++;
+        } while (candidate[1] != START);
 
     }
 
-    public static String HashString(char[] password) {
-        return "string";
+    private static boolean matches(byte[] bytes, byte[] hash) {
+        return false;
+    }
+
+    private static byte[] toByteArray(String s) {
+        return new byte[0];
+    }
+
+    public static byte[] toHashString(char[] password) {
+        return null;
     }
 }
