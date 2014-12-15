c415a80198fa12b98aac077d611a6a0c0a6ddda6
Vance Miller <vbmiller@cs.unc.edu>
2014-12-15 14:40:56 -0500

    it runs

--- Crack.java
+++ Crack.java
@@ -1,30 +1,30 @@
 public class Crack {
-    private static Char START = 'a';
-    private static Char STOP = 'z';
-    private int NUM_CHARS = 5;
+    private static char START = 'a';
+    private static char STOP = 'z';
+    private static int NUM_CHARS = 5;
 
 
     public static void main(String[] args) {
         // create and initialize the password string
-        Char[] candidate = new Char[NUM_CHARS];
+        char[] candidate = new char[NUM_CHARS];
         for (int i = 0; i < candidate.length; i++) {
             candidate[i] = START;
         }
 
         // create loop variables for an arbitrary number of characters in the password string
-        Char[] loopVars = new Char[candidate.length];
+        char[] loopVars = new char[candidate.length];
         for (int i = 0; i < loopVars.length; i++) {
             loopVars[i] = START;
         }
 
         // for each character in the password
         for (int i = 0; i < candidate.length; i++) {
-
+            //
         }
 
     }
 
-    public static String HashString(String[]) {
+    public static String HashString(char[] password) {
         return "string";
     }
 }
