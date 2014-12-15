6205e5854720594f559e5f4d8e902fcc6bdd5adb
Vance Miller <vbmiller@cs.unc.edu>
2014-12-15 15:42:30 -0500

    something

--- Crack.java
+++ Crack.java
@@ -1,10 +1,14 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Crack {
     private static char START = 'a';
     private static char STOP = 'z';
     private static int NUM_CHARS = 5;
     char a = 277;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         byte[] hash;
 
         if (args.length == 1) {
@@ -13,44 +17,46 @@ public class Crack {
             hash = toByteArray("98a2966d4d87b3b62b2b9241e3d517ff");
         }
         // create and initialize the password string
-        char[] candidate = new char[NUM_CHARS];
-        for (int i = 0; i < candidate.length; i++) {
-            candidate[i] = START;
+        char[] iteration_vars = new char[NUM_CHARS];
+        for (int i = 0; i < iteration_vars.length; i++) {
+            iteration_vars[i] = START;
         }
 
         // create loop variables for an arbitrary number of characters in the password string
-        char[] loopVars = new char[candidate.length];
+        char[] loopVars = new char[iteration_vars.length];
         for (int i = 0; i < loopVars.length; i++) {
             loopVars[i] = START;
         }
 
         // for each character in the password
         // this should probably be refactored into something recursive
+
+        iteration_vars[0] = START;
         outerLoop:
         do {
-            candidate[1] = START;
+            iteration_vars[1] = START;
             do {
-                candidate[2] = START;
+                iteration_vars[2] = START;
                 do {
-                    candidate[3] = START;
+                    iteration_vars[3] = START;
                     do {
-                        candidate[4] = START;
+                        iteration_vars[4] = START;
                         do {
-                            candidate[5] = START;
+                            String candidate = "" + iteration_vars[0] + iteration_vars[1] + iteration_vars[2] + iteration_vars[3] + iteration_vars[4];
                             if (matches(toHashString(candidate), hash)) {
-                                System.out.printf("%s\n", candidate);
+                                System.out.printf("%s\n", iteration_vars);
                                 break outerLoop;
                             }
-                            candidate[5]++;
-                        } while (candidate[5] != START);
-                        candidate[4]++;
-                    } while (candidate[4] != START);
-                    candidate[3]++;
-                } while (candidate[3] != START);
-                candidate[2]++;
-            } while (candidate[2] != START);
-            candidate[1]++;
-        } while (candidate[1] != START);
+                            iteration_vars[4]++;
+                        } while (iteration_vars[4] != START);
+                        iteration_vars[3]++;
+                    } while (iteration_vars[3] != START);
+                    iteration_vars[2]++;
+                } while (iteration_vars[2] != START);
+                iteration_vars[1]++;
+            } while (iteration_vars[1] != START);
+            iteration_vars[0]++;
+        } while (iteration_vars[0] != START);
     }
 
     private static boolean matches(byte[] bytes, byte[] hash) {
@@ -66,7 +72,9 @@ public class Crack {
         return javax.xml.bind.DatatypeConverter.parseHexBinary(s);
     }
 
-    public static byte[] toHashString(char[] c) {
-        return null;
+    public static byte[] toHashString(String s) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        byte[] message = s.getBytes("ASCII");
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        return md.digest(message);
     }
 }
