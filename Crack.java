fb6a6fb34ed7eb1e5554e30581b006e910b03e6f
Vance Miller <vbmiller@cs.unc.edu>
2014-12-15 14:21:52 -0500

    character array for the password

--- Crack.java
+++ Crack.java
@@ -1,6 +1,21 @@
 public class Crack {
+    private static Char START = 'a';
+    private static Char STOP = 'z';
+    private int NUM_CHARS = 5;
 
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
+        // create and initialize the password string
+        Char[] candidate = new Char[NUM_CHARS];
+        for (int i = 0; i < candidate.length; i++) {
+            candidate[i] = START;
+        }
+
+        Char loop = start;
+
+        while (loop != stop) {
+
+        }
 
     }
 
